3c0b883ba0fe7a0acd2e677f3e9ed8f8f228661b
VestOfHolding <moviestiles@gmail.com>
2021-10-23 14:01:27 -0700

    Simplifying conditions into lists.

--- dataEntry.py
+++ dataEntry.py
@@ -10,6 +10,12 @@ SKILLS = r"(Perception|Acrobatics|Arcana|Athletics|Crafting|Deception|Diplomacy|
 
 CONDITION_COMPENDIUM = r"@Compendium[pf2e.conditionitems."
 
+CONDITIONS = ["Blinded", "Fatigued", "Confused", "Concealed", "Dazzled", "Deafened", "Invisible",
+              "Flat-Footed", "Immobilized", "Prone", "Unconscious", "Fascinated", "Paralyzed",
+              "Hidden", "Quickened", "Fleeing", "Restrained", "Grabbed"]
+NUMBERED_CONDITIONS = ["Clumsy", "Doomed", "Drained", "Enfeebled", "Slowed", "Frightened", "Sickened",
+                       "Stunned", "Stupefied", "Quickened"]
+
 
 def convert_to_lower(match_obj):
     if match_obj.group() is not None:
@@ -28,38 +34,15 @@ def condition_sub_with_stage(string, condition, stage):
 
 def handle_conditions(string):
     # Condition handling
-    string = condition_sub(string, r"Blinded")
-    string = condition_sub(string, r"Fatigued")
-    string = condition_sub(string, r"Confused")
-    string = condition_sub(string, r"Concealed")
-    string = condition_sub(string, r"Dazzled")
-    string = condition_sub(string, r"Invisible")
-    string = condition_sub(string, r"Flat-Footed")
-    string = condition_sub(string, r"Immobilized")
-    string = condition_sub(string, r"Prone")
-    string = condition_sub(string, r"Unconscious")
-    string = condition_sub(string, r"Fascinated")
-    string = condition_sub(string, r"Paralyzed")
-    string = condition_sub(string, r"Hidden")
-    string = condition_sub(string, r"Quickened")
-    string = condition_sub(string, r"Fleeing")
-    string = condition_sub(string, r"Restrained")
-    string = condition_sub(string, r"Grabbed")
+    for condition in CONDITIONS:
+        string = condition_sub(string, condition)
 
     # Handle this one manually due to the lack of hyphen.
     string = re.sub(r"flat footed", r"%sFlat-Footed]{Flat-Footed}" % CONDITION_COMPENDIUM, string, count=1)
 
-    for i in range(1, 4):
-        string = condition_sub_with_stage(string, r"Clumsy", i)
-        string = condition_sub_with_stage(string, r"Doomed", i)
-        string = condition_sub_with_stage(string, r"Drained", i)
-        string = condition_sub_with_stage(string, r"Enfeebled", i)
-        string = condition_sub_with_stage(string, r"Slowed", i)
-        string = condition_sub_with_stage(string, r"Frightened", i)
-        string = condition_sub_with_stage(string, r"Sickened", i)
-        string = condition_sub_with_stage(string, r"Stunned", i)
-        string = condition_sub_with_stage(string, r"Stupefied", i)
-        string = condition_sub_with_stage(string, r"Quickened", i)
+    for condition in NUMBERED_CONDITIONS:
+        for i in range(1, 4):
+            string = condition_sub_with_stage(string, condition, i)
     return string
 
 
