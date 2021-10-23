7aa9f2959b40766be442cbc584e22945c34525aa
VestOfHolding <moviestiles@gmail.com>
2021-10-22 19:06:48 -0700

    Fixing a test.

--- test_dataEntry.py
+++ test_dataEntry.py
@@ -8,6 +8,6 @@ class Test(TestCase):
     def test_condition_sub(self):
         test_condition = r"Blinded"
         test_string = r"something %s stuff" % test_condition
-        
+
         result = dataEntry.condition_sub(test_string, test_condition)
-        self.assertEquals(r"something @Compendium[pf2e.conditionitems.Blinded]{Blinded}", result)
+        self.assertEquals(r"something @Compendium[pf2e.conditionitems.Blinded]{Blinded} stuff", result)
